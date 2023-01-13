54dfeb9d41a546d01c456e9f90db5cd9834deea2
Dario Kiramarios <dk119124660@fer.hr>
2023-01-13 17:24:28 +0100

    Mini ispravak

--- IzvorniKod/Frontend/dzd-app/src/components/MojaDonacijaKard.jsx
+++ IzvorniKod/Frontend/dzd-app/src/components/MojaDonacijaKard.jsx
@@ -426,13 +426,13 @@ const MojeDonacijaKard = (props) => {
                       onChange={(e) => setSpol(e.target.value)}>
                       <div>
                         <FormControlLabel
-                          value="m"
+                          value="z"
                           disabled={smijeMijenjati}
                           control={<Radio />}
                           label="Žensko"
                         />
                         <FormControlLabel
-                          value="z"
+                          value="m"
                           disabled={smijeMijenjati}
                           control={<Radio />}
                           label="Muško"
